36f6eada56ad8c3947007b6b221af93480939b1c
EmJayAA <martinjonas.aranzado@dbtc-cebu.edu.ph>
2026-05-01 21:08:05 +0800

    feat: Implement CourseRepository and ICourseRepository

---
+++ AMS_Backend_V2/AMS_Backend_V2/Repositories/CourseRepo/CourseRepository.cs
@@ -0,0 +1,40 @@
+﻿using AMS_Backend_V2.Data;
+using AMS_Backend_V2.Models;
+using Microsoft.EntityFrameworkCore;
+namespace AMS_Backend_V2.Repositories.CourseRepo
+{
+    public class CourseRepository : ICourseRepository
+    {
+        private readonly AttendanceDbContext _context;
+        public CourseRepository(AttendanceDbContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Course>> GetAllAsync()
+        {
+            return await _context.Courses.ToListAsync();
+        }
+        public async Task<Course> GetByIdAsync(int id)
+        {
+            return await _context.Courses.FindAsync(id);
+        }
+        public async Task AddAsync(Course course)
+        {
+            await _context.Courses.AddAsync(course);
+        }
+        public async Task UpdateAsync(Course course)
+        {
+            _context.Courses.Update(course);
+            await _context.SaveChangesAsync();
+        }
+        public async Task DeleteAsync(int id)
+        {
+            var course = _context.Courses.Find(id);
+            if (course != null) 
+            {
+                _context.Courses.Remove(course);
+                await _context.SaveChangesAsync();
+            }
+        }
+    }
+}
