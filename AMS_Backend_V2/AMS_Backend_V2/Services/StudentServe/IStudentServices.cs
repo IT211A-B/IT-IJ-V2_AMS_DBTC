fa521b396ded228ddf3025f3077a9afa5454cc6a
EmJayAA <martinjonas.aranzado@dbtc-cebu.edu.ph>
2026-05-02 08:13:04 +0800

    feat: Implemented StudentServices and IStudentServices

---
+++ AMS_Backend_V2/AMS_Backend_V2/Services/StudentServe/IStudentServices.cs
@@ -0,0 +1,13 @@
+﻿using AMS_Backend_V2.DTOs;
+
+namespace AMS_Backend_V2.Services.StudentServe
+{
+    public interface IStudentServices
+    {
+        Task<IEnumerable<StudentDto.ReadStudentDto>> GetAllStudentsAsync();
+        Task<StudentDto.ReadStudentDto> GetByStudentIdAsync(int id);
+        Task CreateStudentAsync(StudentDto.CreateStudentDto studentDto);
+        Task UpdateStudentAsync(StudentDto.UpdateStudentDto studentDto);
+        Task DeleteStudentAsync(int id);
+    }
+}
---
+++ AMS_Backend_V2/AMS_Backend_V2/Services/StudentServe/StudentServices.cs
@@ -0,0 +1,65 @@
+﻿using AMS_Backend_V2.Models;
+using AMS_Backend_V2.DTOs;
+using AMS_Backend_V2.Enums;
+using AMS_Backend_V2.Repositories.StudentRepo;
+using System.ComponentModel.DataAnnotations;
+namespace AMS_Backend_V2.Services.StudentServe
+{
+    public class StudentService : IStudentServices
+    {
+        private readonly IStudentRepository _studentRepo;
+        public StudentService(IStudentRepository studentRepo)
+        {
+            _studentRepo = studentRepo;
+        }
+        public async Task<IEnumerable<StudentDto.ReadStudentDto>> GetAllStudentsAsync()
+        {
+            var students = await _studentRepo.GetAllStudentsAsync();
+            return students.Select(s => new StudentDto.ReadStudentDto
+            {
+                StudentId = s.StudentId,
+                FullName = s.FirstName + " " + s.LastName
+            }).ToList();
+        }
+        public async Task<StudentDto.ReadStudentDto> GetByStudentIdAsync(int id)
+        {
+            var students = await _studentRepo.GetByStudentIdAsync(id);
+            if (students == null)
+            {
+                return null;
+            }
+            return new StudentDto.ReadStudentDto
+            {
+                StudentId = students.StudentId,
+                FullName = students.FirstName + " " + students.LastName
+            };
+        }
+        public async Task CreateStudentAsync(StudentDto.CreateStudentDto studentDto)
+        {
+            var student = new Student
+            {
+                FirstName = studentDto.FirstName,
+                LastName = studentDto.LastName,
+                Sex = studentDto.Sex,
+                Email = studentDto.Email
+            };
+            await _studentRepo.AddStudentAsync(student);
+        }
+        public async Task UpdateStudentAsync(StudentDto.UpdateStudentDto studentDto)
+        {
+            var eStudent = await _studentRepo.GetByStudentIdAsync(studentDto.StudentId);
+            if (eStudent != null)
+            {
+                eStudent.FirstName = studentDto.FirstName;
+                eStudent.LastName = studentDto.LastName;
+                eStudent.Sex = studentDto.Sex;
+                eStudent.Email = studentDto.Email;
+                await _studentRepo.UpdateStudentAsync(eStudent);
+            }
+        }
+        public async Task DeleteStudentAsync(int id)
+        {
+            await _studentRepo.DeleteStudentAsync(id);
+        }
+    }
+}
