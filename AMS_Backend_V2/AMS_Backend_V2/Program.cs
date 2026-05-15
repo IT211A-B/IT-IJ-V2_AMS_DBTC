581f85ddc05a7db5a31574850647ce08f9d78439
EmJayAA <martinjonas.aranzado@dbtc-cebu.edu.ph>
2026-05-15 11:43:13 +0800

    fix: fix placement of AddCors method for Frontend in Program.cs

--- AMS_Backend_V2/AMS_Backend_V2/Program.cs
+++ AMS_Backend_V2/AMS_Backend_V2/Program.cs
@@ -17,7 +17,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy("AllowFrontend",
         policy =>
         {
-            policy.WithOrigins("http://localhost:3000", "http://127.0.0.1:5500") // Add your frontend URLs here
+            policy.WithOrigins("http://localhost:3000", "http://127.0.0.1:5500") 
                   .AllowAnyHeader()
                   .AllowAnyMethod();
         });
